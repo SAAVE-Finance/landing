62bfdd08a20de0e0a4be5908447c2b3275ac129e
Pratham <prathamsinghal2003@gmail.com>
2023-02-09 00:45:59 +0530

    :art: revamped navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="w-full flex  text-white font-['DM Sans'] justify-between my-5 py-8 px-20">
+      <div className=" text-xl">
+        <h2>CRONOS</h2>
+      </div>
+      <div className="flex justify-around ">
+        <a className="cursor-pointer mx-10">Learn</a>
+        <a className="cursor-pointer mx-10">Build</a>
+        <a className="cursor-pointer mx-10">Explore</a>
+      </div>
+      <button className="hover:scale-105">
+        <Link
+          href="https://frontend-olyn.vercel.app"
+          className="cursor-pointer bg-white text-black p-4 hover:scale-125 "
+        >
+          Get Cronos &gt;
+        </Link>
+      </button>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h2 className="text-white">HII</h2>
+        <h2 className="text-white text-5xl">HII</h2>
       </main>
     </>
   );
