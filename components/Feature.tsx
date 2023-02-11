a805d14f55fa3ec61d109db56e79e601caa5553b
Pratham <prathamsinghal2003@gmail.com>
2023-02-12 01:14:23 +0530

    :rocket: Updated feature section

--- components/Feature.tsx
+++ components/Feature.tsx
@@ -9,45 +9,24 @@ import Badge from "@/public/assets/image 3.png";
 const Feature = () => {
   return (
     <div className="mt-48 ">
-      <h1 className="font-normal text-6xl text-center leading-[64px] text-transparent bg-clip-text bg-gradient-to-r from-white to-[rgba(255,255,255,0.45)] mx-auto">
-        Be Part of the Open
+      <h1 className="font-normal text-6xl text-center leading-[64px] font-[inter] text-white my-5 ">
+        What does SAAVE do?
       </h1>
-      <h1 className="font-normal text-6xl text-center leading-[64px] text-transparent bg-clip-text bg-gradient-to-r from-white to-[rgba(255,255,255,0.45)] mx-auto mb-20">
-        Economy of the Future.
-      </h1>
-      <div className="flex  flex-col align-middle flex-wrap ">
-        <div className="flex flex-row justify-center">
-          <Card
-            img={Coin}
-            title={"Marketplace"}
-            description={
-              "Set to operate a next-gen decentralized exchange, swapping digital assets from across the Interchain, with very low fees and instant transaction confirmation."
-            }
-          />
-          <Card
-            img={Sheild}
-            title={"Security provider"}
-            description={
-              "With the upcoming Interchain Security feature, HEDRON will soon be securing many chains, in exchange for additional staking rewards."
-            }
-          />
-        </div>
-        <div className="flex flex-row justify-center">
-          <Card
-            img={Chart}
-            title={"Router"}
-            description={
-              "A core mission of the Hub – to connect chains by establishing IBC connections with compatible chains with chains like Ethereum and Bitcoin."
-            }
-          />
-          <Card
-            img={Badge}
-            title={"Custodian"}
-            description={
-              "A core mission of the Hub – to connect chains by establishing IBC connections with compatible chains and operating chains like Ethereum and Bitcoin."
-            }
-          />
-        </div>
+      <div className="flex flex-row justify-center">
+        <Card
+          img={Coin}
+          title={"Marketplace"}
+          description={
+            "Set to operate a next-gen decentralized exchange, swapping digital assets from across the Interchain, with very low fees and instant transaction confirmation."
+          }
+        />
+        <Card
+          img={Sheild}
+          title={"Security provider"}
+          description={
+            "With the upcoming Interchain Security feature, HEDRON will soon be securing many chains, in exchange for additional staking rewards."
+          }
+        />
       </div>
     </div>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,28 +5,27 @@ import Shape1 from "@/public/assets/ecto.png";
 const Hero = () => {
   return (
     <>
-      <main className="relative flex flex-col  text-white w-[85vw] mx-auto my-16 z-50 ">
-        <h2 className={`${styles.heading} text-[#ffffff99] my-10 mx-auto`}>
-          WELCOME TO CRONOS
+      <main className="relative flex flex-col  text-white align-middle my-16 z-50 ">
+        <h2 className="font-DM_Sans text-[#ffffff99] my-10 mx-auto">
+          WELCOME TO SAAVE
         </h2>
-        <h1 className="text-8xl  text-transparent bg-clip-text bg-gradient-to-r from-white to-[rgba(255,255,255,0.45)] mx-auto font-extrabold font-Inter">
+        <h1
+          className={`text-8xl text-white mx-auto font-extrabold font-[Inter] ${styles.heading}`}
+        >
           The Internent of
         </h1>
-        <h1 className="text-8xl  text-transparent bg-clip-text bg-gradient-to-r from-white to-[rgba(255,255,255,0.45)] mx-auto">
+        <h1
+          className={`text-8xl text-white mx-auto font-extrabold font-[Inter] ${styles.heading}`}
+        >
           Blockchain
         </h1>
-        <p
-          className={`${styles.heading} my-10  mx-auto w-[70%] text-[#ffffffaf] text-center`}
-        >
+        <p className="font-DM_Sans my-10  mx-auto w-[50%] text-[#ffffffaf] text-center">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
           incidunt quas totam sapiente voluptatem natus cupiditate
         </p>
       </main>
-      <div className="absolute z-1 h-10 top-48 left-36">
-        <Image src={Shape1} alt="hero" width={200} />
-      </div>
       <div className="absolute z-1 h-10 bottom-52 -right-20">
-        <Image src={Shape1} alt="hero" />
+        <Image src={Shape1} alt="hero" width={400} />
       </div>
     </>
   );
